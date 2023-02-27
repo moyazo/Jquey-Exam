02b41bed5a2c69eda8341ab1511475ccb0320d21
Manuel Moya <manuel.moyavadillo2@gmail.com>
2023-02-27 23:40:09 +0100

    feat: all completed ☺

--- js/custom.js
+++ js/custom.js
@@ -1,44 +1,43 @@
-"use strict"
+"use strict";
 $("#app").hide();
 $("#loader").show();
-$( window ).ready(function(){
+$(window).ready(function () {
     setTimeout(() => {
-                $("#app").show();
-                $("#loader").hide();
-            },2000)
-});+
-
-$(document).ready(function(){
-    $(".form").click(function(){
-        confirm("THIS FORM IS DISABLE")
+        $("#app").show();
+        $("#loader").hide();
+    }, 2000);
+});
++$(document).ready(function () {
+    $(".form").click(function () {
+        confirm("THIS FORM IS DISABLE");
     });
-    $(".btn1").click(function(){
+    $(".btn1").click(function () {
         $(".btn2").hide();
         $(".btn3").hide();
     });
-    $(".btn2").click(function(){
+    $(".btn2").click(function () {
         $(".btn1").hide();
         $(".btn3").hide();
     });
-    $(".btn3").click(function(){
+    $(".btn3").click(function () {
         $(".btn2").hide();
         $(".btn1").hide();
     });
-    $(".save").click(function(){
+    $(".save").click(function () {
         $(".btn1").show();
         $(".btn2").show();
         $(".btn3").show();
     });
-    $(".close").click(function(){
+    $(".close").click(function () {
         $(".btn1").show();
         $(".btn2").show();
         $(".btn3").show();
     });
     $("#pointer").hide();
-    $("#servicesTitle").click(function(){
+    $("#servicesTitle").click(function () {
         $("#services").fadeToggle();
     });
-    $("h4").click(function(){
+    $("h4").click(function () {
         $(".carousel-inner").slideToggle("slow");
         $(".carousel-control-next").hide();
         $(".carousel-control-prev").hide();
