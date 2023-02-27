f6915596e76b01dc7308a08d9f6bc68425757a65
Manuel Moya <manuel.moyavadillo2@gmail.com>
2023-02-27 23:11:51 +0100

    feat: jquery added

---
+++ js/custom.js
@@ -0,0 +1,52 @@
+"use strict"
+// window.addEventListener('load',() => {
+//     $("#app").hide();
+//     $("#loader").show();
+//     setTimeout(() => {
+//         $("#app").show();
+//         $("#loader").hide();
+//     },2000)
+// });
+$("#app").hide();
+$("#loader").show();
+$( document ).ready(function(){
+    setTimeout(() => {
+                $("#app").show();
+                $("#loader").hide();
+            },2000)
+});
+
+$(document).ready(function(){
+    $(".btn1").click(function(){
+        $(".btn2").hide();
+        $(".btn3").hide();
+    });
+    $(".btn2").click(function(){
+        $(".btn1").hide();
+        $(".btn3").hide();
+    });
+    $(".btn3").click(function(){
+        $(".btn2").hide();
+        $(".btn1").hide();
+    });
+    $(".save").click(function(){
+        $(".btn1").show();
+        $(".btn2").show();
+        $(".btn3").show();
+    });
+    $(".close").click(function(){
+        $(".btn1").show();
+        $(".btn2").show();
+        $(".btn3").show();
+    });
+    $("#pointer").hide();
+    $("#servicesTitle").click(function(){
+        $("#services").fadeToggle();
+    });
+    $("h4").click(function(){
+        $(".carousel-inner").slideToggle("slow");
+        $(".carousel-control-next").hide();
+        $(".carousel-control-prev").hide();
+        $(".carousel-indicators").hide();
+    });
+});
